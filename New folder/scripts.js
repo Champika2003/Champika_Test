4473e0cddef393612e56ac6ee2756951d23c5572
Champika2003 <champikasandaruwan111999@gmail.com>
2024-06-19 17:42:45 +0530

    Add files via upload

---
+++ New folder/scripts.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+    // Add any JavaScript code for interactive features
+});
